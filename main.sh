9f99435745198798ef76c3dcf479d4088f07a9a6
ferrreo <harderthanfire@gmail.com>
2023-06-11 15:02:24 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,9 +9,11 @@ add-apt-repository ppa:pikaos/pika
 add-apt-repository ppa:kubuntu-ppa/backports
 apt install pika-sources.deb --yes --option Acquire::Retries=5 --option Acquire::http::Timeout=100 --option Dpkg::Options::="--force-confnew"
 # Clone Upstream
-mkdir -p ./src-pkg-name
-cp -rvf ./debian ./src-pkg-name/
-cd ./src-pkg-name/
+# Clone Upstream
+git clone --recurse-submodules https://github.com/ErikReider/SwayOSD
+cd SwayOSD
+cp -rvf ../debian ./
+cp -vf ../Makefile ./Makefile
 
 # Get build deps
 ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
